3651abe1fc78f3722efe76335dc8afff8f76f603
Peter Law <PeterJCLaw@gmail.com>
2019-11-23 15:12:09 +0000

    Clarify that SR is now the author

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ except ImportError:
 
 config = {
     'description': 'libnemesis',
-    'author': 'Sam Phippen',
-    'url': 'http://github.com/samphippen/libnemesis',
-    'download_url': 'http://github.com/samphippen/libnemesis',
-    'author_email': 'samphippen@googlemail.com',
+    'author': "Student Robotics Competitor Services Team",
+    'url': 'https://github.com/srobo/libnemesis',
+    'download_url': 'https://github.com/srobo/libnemesis',
+    'author_email': 'srobo-devel@googlegroups.com',
     'version': '0.0.1',
     'install_requires': ['nose', 'python-ldap', 'Unidecode'],
     'packages': ['libnemesis'],
